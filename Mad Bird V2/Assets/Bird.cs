2a7758ba31be6208c9da3bb50e770625f45d8292
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2020-10-28 02:22:22 -0500

    2 hour 6 min of video done
    2 hour 6 min of video done

--- Mad Bird V2/Assets/Bird.cs
+++ Mad Bird V2/Assets/Bird.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 public class Bird : MonoBehaviour
 {
     Vector3 _initialPosition;
+    [SerializeField]private float _launchPower = 250;
+    private bool _BirdWasLaunched;
+    private float _TimeSittingAround;
 
     private void Awake()
     {
@@ -12,21 +15,41 @@ public class Bird : MonoBehaviour
 
     private void Update()
     {
-        if (transform.position.y > 10) ;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GetComponent<LineRenderer>().SetPosition(0, _initialPosition);
+        GetComponent<LineRenderer>().SetPosition(1, transform.position);
+
+        if (_BirdWasLaunched &&
+            GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _TimeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 ||
+            transform.position.y < -10 ||
+            transform.position.x > 10 ||
+            transform.position.x < -10 ||
+            _TimeSittingAround > 1)
+
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
     }
 
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
     }
 
     private void OnMouseUp()
     {
         GetComponent<SpriteRenderer>().color = Color.white;
         Vector2 directionToInitialPosition = _initialPosition - transform.position;
-        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
+        _BirdWasLaunched = true;
+        GetComponent<LineRenderer>().enabled = false;
     }
 
     private void OnMouseDrag()
