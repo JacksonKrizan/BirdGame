cb4ef9b3ef44b2bf41d2cac714203b6a10864eda
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2020-10-28 21:42:38 -0500

    1 hour 50 min
    1 hour 50 min

---
+++ Mad Bird V2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrfab;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Bird>() != null)
+        {
+            Instantiate(_cloudParticlePrfab,transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+        if (collision.contacts[0].normal.y < - 0.5)
+        {
+            Instantiate(_cloudParticlePrfab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
