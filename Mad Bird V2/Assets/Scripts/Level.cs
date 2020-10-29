061a1f331fe500859f0056e521b7f9126435300b
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2020-10-29 16:06:34 -0500

    2hour 58min

--- Mad Bird V2/Assets/Scripts/Level.cs
+++ Mad Bird V2/Assets/Scripts/Level.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Enemy [] _enemies;
 
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(Enemy enemy in _enemies) 
+        {
+            if (enemy!= null)
+                {
+                    return;
+                }
+
+            Debug.Log("You killed all Enemies");
+
+            //string nextlevelName = ""
+        }
     }
 }
